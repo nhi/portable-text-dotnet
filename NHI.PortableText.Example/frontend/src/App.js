5c1f9b6898aded2798d76214dedb3df898572c5f
Petter Rein <petter.rein@nhi.no>
2020-10-01 10:11:48 +0200

    fix test that fail if react is not imported

--- NHI.PortableText.Example/frontend/src/App.js
+++ NHI.PortableText.Example/frontend/src/App.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import BlockContent from "@sanity/block-content-to-react"
 import axios from "axios"
